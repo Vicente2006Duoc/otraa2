686c911cab45bf7b5999ef6558924e53d5fece9c
Vicente2006Duoc <vi.quilaleo@duocuc.cl>
2025-07-02 11:06:32 -0400

    Add files via upload

---
+++ calculadora promedio/promedio2.py
@@ -0,0 +1,8 @@
+notas = []
+cantidad = int(input("ingreese la cantidad de notas: "))
+for i in range(cantidad):
+    nota = float(input(f"ingrese la nota numero {i + 1} :"))
+    notas.append(nota)
+    
+    promedio = sum(notas) / len(notas)
+    print("el promedio final es {promedio:2f}")
